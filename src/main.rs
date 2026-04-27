e9cbc141e058dae07bb76b84c6b72825fbed0feb
mubeen <mohdmubeen5042@gmail.com>
2026-04-27 22:54:42 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,12 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment the code below to pass the first stage
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+
+    println!("Logs from your program will appear here!");
     for stream in listener.incoming() {
+
+    println!("Logs from your program will appear here!");
         thread::spawn( || {
             match stream {
                 Ok(mut stream) => { 
@@ -21,9 +24,10 @@ fn main() {
                         }
 
                         let input = String::from_utf8_lossy(&buff[..bytes_read]);
+                        println!("{:?} this is input" , input);
                         let response = handle_command(&input);
 
-                        stream.write_all(response.as_bytes()).unwrap()
+                        //stream.write_all(response.as_bytes()).unwrap()
                     }
                 }
                 Err(e) => {
@@ -35,8 +39,19 @@ fn main() {
 }
 
 fn handle_command(input : &str) -> String{
-   let input =  input.trim();
+   let (first , second ) = input.split_at(5);
 
-   let Some((first , second)) = input.split_once(' ')
-    
+   if first.to_lowercase() == "ping" {
+        return  "PONG".to_string();
+   }
+
+   if first.to_lowercase() == "echo" {
+       let length_of_string = second.len();
+       let result = format!("{length_of_string}\r\n{second}\r\n");
+       return result
+   }
+
+
+   "".to_string()
 }
+
